8bd10916e13ccc7211a27031c88d4cc523d1585a
Leo Zheng <leozheng0901@gmail.com>
2025-01-12 19:00:57 -0800

    Config

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,12 @@
 import adapter from '@sveltejs/adapter-node';
+import { sveltekit } from '@sveltejs/kit/vite';
 
 export default {
+plugins: [sveltekit()],
   kit: {
     adapter: adapter(),
+  },
+  server: {
+    hmr: process.env.NODE_ENV === 'production', // Enable HMR only in development
   },
 };
