0f50e0311471100789f49420ebb39b9f25ac675f
Leo Zheng <leozheng0901@gmail.com>
2025-01-12 21:24:03 -0800

    packages refresh" "

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,18 @@
 import adapter from '@sveltejs/adapter-node';
-import { sveltekit } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-export default {
-plugins: [sveltekit()],
-  kit: {
-    adapter: adapter(),
-  },
-  server: {
-    hmr: process.env.NODE_ENV === 'production', // Enable HMR only in development
-  },
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	kit: {
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter()
+	}
 };
+
+export default config;
