3df29eacbe440a9e6c84c9991aa429425ab4383f
Leo Zheng <leozhengcs@gmail.com>
2025-03-12 15:16:52 -0700

    Change config file

--- svelte.config.js
+++ svelte.config.js
@@ -4,9 +4,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 export default {
 plugins: [sveltekit()],
   kit: {
-    adapter: adapter(),
-  },
-  server: {
-    hmr: process.env.NODE_ENV === 'production', // Enable HMR only in development
-  },
+    adapter: adapter({
+      // This configures where the static build output goes
+      pages: 'public',       // HTML/JS/CSS files
+      assets: 'public',      // Static assets
+      fallback: 'index.html' // SPA fallback
+    })  },
+  // server: {
+  //   hmr: process.env.NODE_ENV === 'production', // Enable HMR only in development
+  // },
+
 };
