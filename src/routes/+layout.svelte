ba2e2eb25a963b2e68e42c40af5933857a195c2b
Leo Zheng <leozhengcs@gmail.com>
2025-04-01 21:07:00 -0700

    Add Vercel Analytics injection

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,10 @@
 	import Footer from '$lib/components/Footer.svelte';
 	import Header from '$lib/components/Header.svelte';
 	import { footerState } from '../stores/footer.svelte';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit'
 
 	let { children } = $props();
+	injectAnalytics();
 </script>
 
 <Header />
